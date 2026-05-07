3da0593fbb8bc46968a4688e4a94982436584b0a
Ssalijoshua <ssalijoshua2002@gmail.com>
2026-05-07 10:48:50 +0300

    Added exercise 2

---
+++ exercise2.js
@@ -0,0 +1,26 @@
+// EXERCISE
+
+// Question 1: You are required to create 3 arrays. One array called Student usernames, another array called Student passwords and another called Student ages. The three arrays should have the same number of elements. 
+user = {
+    username: "Angel",
+    password: "tszj12345678",
+    Year_of_birth: "2006"
+}
+ 
+user2 = {
+    username: "Emma",
+    password: "12345678",
+    Year_of_birth: "1994"
+}
+
+user3 = {
+    username: "Druscillah",
+    password: "5678",
+    Year_of_birth: "2000"
+}
+
+user4 ={
+    username: "HOSHI",
+    password: "2026",
+    Year_of_birth: "1990"
+}
