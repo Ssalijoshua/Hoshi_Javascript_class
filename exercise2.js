40a8dc9d334ee22a8cdc11c10b071ac484e83f31
Ssalijoshua <ssalijoshua2002@gmail.com>
2026-05-07 11:55:55 +0300

    Added question 2 and 3

--- exercise2.js
+++ exercise2.js
@@ -1,6 +1,8 @@
 // EXERCISE
 
 // Question 1: You are required to create 3 arrays. One array called Student usernames, another array called Student passwords and another called Student ages. The three arrays should have the same number of elements. 
+
+
 user = {
     username: "Angel",
     password: "tszj12345678",
@@ -24,3 +26,11 @@ user4 ={
     password: "2026",
     Year_of_birth: "1990"
 }
+
+
+// Question 2
+// Write a function that takes an array with duplicate values (e.g., [1, 2, 2, 3]) and returns a new array with only unique values.
+
+
+// Question 3
+//  Write a function that comapres 2 arrrays and returns true if they are simnilar and false if they arent
